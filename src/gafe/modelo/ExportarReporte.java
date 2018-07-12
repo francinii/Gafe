2e997511146c971d180091f9d6383e2e5f1d4a54
HP_810G2 <HP_810G2@DESKTOP-HFVR94I>
2018-07-12 10:28:41 -0600

    Exporta a excel

--- src/gafe/modelo/ExportarReporte.java
+++ src/gafe/modelo/ExportarReporte.java
@@ -5,12 +5,13 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.util.List;
 import javax.swing.JTable;
- import jxl.Workbook;
- import jxl.write.Label;
- import jxl.write.WritableSheet;
- import jxl.write.WritableWorkbook;
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
 
 public class ExportarReporte {
+
     private File file;
     private List<JTable> tabla;
     private List<String> nom_files;
@@ -28,22 +29,32 @@ public class ExportarReporte {
         try {
             DataOutputStream out = new DataOutputStream(new FileOutputStream(file));
             WritableWorkbook w = Workbook.createWorkbook(out);
-            for (int index = 0; index < tabla.size(); index++) {
-                JTable table = tabla.get(index);
-                WritableSheet s = w.createSheet(nom_files.get(index), 0);
-                for (int i = 0; i < table.getColumnCount(); i++) { 
-                     s.addCell(new Label(i, 0,  table.getColumnName(i)));
+            // for (int index = 0; index < tabla.size(); index++) {
+            int index = 0;
+            JTable table = tabla.get(index);
+            WritableSheet s = w.createSheet(nom_files.get(index), 0);
+           int k = 0;
+            for (int i = 0; i < table.getColumnCount(); i++) {
+                if (tabla.get(index).getColumnModel().getColumn(i).getMinWidth() != 0) {
+                    s.addCell(new Label(k, 0, table.getColumnName(i)));
+                    k = k+1;
                 }
-                for (int i = 0; i < table.getColumnCount(); i++) {                   
-                    for (int j = 1; j < table.getRowCount(); j++) {                        
-                        Object object = table.getValueAt(j, i);                        
-                        if(object != null){
-                        s.addCell(new Label(i, j, String.valueOf(object)));
-                        }else {
-                            s.addCell(new Label(i, j, ""));
+            }
+            k = 0;
+            for (int i = 0; i < table.getColumnCount(); i++) {
+                if (tabla.get(index).getColumnModel().getColumn(i).getMinWidth() != 0) {
+                    //ANTES DE ENTRAR A ESTE FOR VERIFICO SI EL SIZE DE LA COLUMNA QUE OBTENGO ES 0 SI ES CERO NO LO MUESTRO Y ME BRINCO LA COLUMNA
+                    for (int j = 0; j < table.getRowCount(); j++) {
+                        Object object = table.getValueAt(j, i);
+                        if (object != null) {
+                            s.addCell(new Label(k, j+1, String.valueOf(object)));
+                        } else {
+                            s.addCell(new Label(k, j+1, "hola"));
                         }
                     }
+                    k = k +1;
                 }
+                //   }
             }
             w.write();
             w.close();
--- src/gafe/vista/formularioReporte.java
+++ src/gafe/vista/formularioReporte.java
@@ -47,7 +47,7 @@ public class formularioReporte extends javax.swing.JPanel {
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, true, false
+                true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, true, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
