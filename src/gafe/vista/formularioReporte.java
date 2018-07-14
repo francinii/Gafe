6fe88e1fa34ec371b7c3ab1b49b17cb4be84a749
Andrey <Andrey@DESKTOP-VEEUK3H>
2018-07-14 16:13:24 -0600

    Merge origin/master
    Conflicts:
    	src/gafe/vista/formularioReporte.form
    	src/gafe/vista/formularioReporte.java

--- src/gafe/vista/formularioReporte.java
+++ src/gafe/vista/formularioReporte.java
@@ -2,6 +2,7 @@ package gafe.vista;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class formularioReporte extends javax.swing.JPanel {
 
@@ -14,6 +15,14 @@ public class formularioReporte extends javax.swing.JPanel {
     
     public JTable tablaReportes(){
         return TablaReportes;
+    }
+    public void limpiarTabla() {
+        DefaultTableModel modelo = (DefaultTableModel) TablaReportes.getModel();
+        int filas = TablaReportes.getRowCount();
+        for (int i = 0; filas > i; i++) {
+            modelo.removeRow(0);
+            System.out.println("Limpiar");
+        }             
     }
 
     @SuppressWarnings("unchecked")
